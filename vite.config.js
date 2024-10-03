3eb09246e79e2b727da35705a7f1effd4cf0d96b
AliAlTaweel <engalihaitham@gmail.com>
2024-10-03 20:46:44 +0300

    pushlishing adding done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/FS-P02FS-P02-data for countries",
+
   plugins: [react()],
-})
+});
